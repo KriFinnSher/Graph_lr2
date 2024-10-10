050f603a5edef375609041976d5680b71f5c56be
Vadim <105079192+KriFinnSher@users.noreply.github.com>
2024-10-10 14:41:30 +0300

    Update main.cpp
    change to rus

--- Graph1/main.cpp
+++ Graph1/main.cpp
@@ -1,4 +1,4 @@
-// Задача 1. Даны две разреженные матрицы CS. Перемножить их и результат занести в разреженную матрицу CRS.
+// Р—Р°РґР°С‡Р° 1. Р”Р°РЅС‹ РґРІРµ СЂР°Р·СЂРµР¶РµРЅРЅС‹Рµ РјР°С‚СЂРёС†С‹ CS. РџРµСЂРµРјРЅРѕР¶РёС‚СЊ РёС… Рё СЂРµР·СѓР»СЊС‚Р°С‚ Р·Р°РЅРµСЃС‚Рё РІ СЂР°Р·СЂРµР¶РµРЅРЅСѓСЋ РјР°С‚СЂРёС†Сѓ CRS.
 
 #include<iostream>
 #include<vector>
@@ -85,7 +85,7 @@ std::vector<Elem> generate_sparse_mat(int row, int col, int count_of_elems) {
 }
 
 void print_sparse_mat(std::vector<Elem> mat, int row, int col) {
-	std::cout << "1 - Вывести матрицу целиком, 2 - вывести в формате head & tail\n";
+	std::cout << "1 - Р’С‹РІРµСЃС‚Рё РјР°С‚СЂРёС†Сѓ С†РµР»РёРєРѕРј, 2 - РІС‹РІРµСЃС‚Рё РІ С„РѕСЂРјР°С‚Рµ head & tail\n";
 	int choice = 0;
 	std::cin >> choice;
 	if (choice == 1) {
@@ -107,7 +107,7 @@ void print_sparse_mat(std::vector<Elem> mat, int row, int col) {
 		}
 	}
 	else if (choice == 2) {
-		std::cout << "Введите значения head и tail через пробел: ";
+		std::cout << "Р’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёСЏ head Рё tail С‡РµСЂРµР· РїСЂРѕР±РµР»: ";
 		int head = 0, tail = 0;
 		std::cin >> head >> tail;
 		for (int i = 0; i < head; ++i) {
@@ -175,7 +175,7 @@ void print_sparse_mat(std::vector<Elem> mat, int row, int col) {
 		}
 	}
 	else
-		std::cout << "Ошибка ввода, перезапустите программу..." << std::endl;
+		std::cout << "РћС€РёР±РєР° РІРІРѕРґР°, РїРµСЂРµР·Р°РїСѓСЃС‚РёС‚Рµ РїСЂРѕРіСЂР°РјРјСѓ..." << std::endl;
 }
 
 void write_custom_to_file(std::string& filename) {
@@ -214,16 +214,16 @@ std::vector<Elem> read_custom_from_file(std::ifstream& mat_file, int row, int co
 }
 
 void print_crs_mat(std::vector<std::vector<int>> mat) {
-	std::cout << "\nИтоговая матрица в формате CRS:\n";
-	std::cout << "Массив значений: { ";
+	std::cout << "\nРС‚РѕРіРѕРІР°СЏ РјР°С‚СЂРёС†Р° РІ С„РѕСЂРјР°С‚Рµ CRS:\n";
+	std::cout << "РњР°СЃСЃРёРІ Р·РЅР°С‡РµРЅРёР№: { ";
 	for (int i = 0; i < mat[0].size(); i++)
 		std::cout << mat[0][i] << " ";
 	std::cout << "}\n";
-	std::cout << "Массив индексов столбцов: { ";
+	std::cout << "РњР°СЃСЃРёРІ РёРЅРґРµРєСЃРѕРІ СЃС‚РѕР»Р±С†РѕРІ: { ";
 	for (int i = 0; i < mat[1].size(); i++)
 		std::cout << mat[1][i] << " ";
 	std::cout << "}\n";
-	std::cout << "Массив индексации строк: { ";
+	std::cout << "РњР°СЃСЃРёРІ РёРЅРґРµРєСЃР°С†РёРё СЃС‚СЂРѕРє: { ";
 	for (int i = 0; i < mat[2].size(); i++)
 		std::cout << mat[2][i] << " ";
 	std::cout << "}\n";
@@ -237,30 +237,30 @@ int main() {
 	std::vector<Elem> mat_2;
 	int rows_1 = 0, columns_1 = 0, count_of_elems = 0, res_row = 0, res_col = 0, rows_2 = 0, columns_2 = 0, fullness = 0;
 
-	std::cout << "\t\t\t\t----------- Информация по программе -----------\n\n"
-		<< "Для работы с матрицами выберете способ их создания:\n\n"
-		<< "1 - произвольная генерация по заданным параметрам\n"
-		<< "2 - построение по заданной конкретной структуре\n\n";
+	std::cout << "\t\t\t\t----------- РРЅС„РѕСЂРјР°С†РёСЏ РїРѕ РїСЂРѕРіСЂР°РјРјРµ -----------\n\n"
+		<< "Р”Р»СЏ СЂР°Р±РѕС‚С‹ СЃ РјР°С‚СЂРёС†Р°РјРё РІС‹Р±РµСЂРµС‚Рµ СЃРїРѕСЃРѕР± РёС… СЃРѕР·РґР°РЅРёСЏ:\n\n"
+		<< "1 - РїСЂРѕРёР·РІРѕР»СЊРЅР°СЏ РіРµРЅРµСЂР°С†РёСЏ РїРѕ Р·Р°РґР°РЅРЅС‹Рј РїР°СЂР°РјРµС‚СЂР°Рј\n"
+		<< "2 - РїРѕСЃС‚СЂРѕРµРЅРёРµ РїРѕ Р·Р°РґР°РЅРЅРѕР№ РєРѕРЅРєСЂРµС‚РЅРѕР№ СЃС‚СЂСѓРєС‚СѓСЂРµ\n\n";
 
 	int choice = 0;
 	std::cin >> choice;
 	std::string file_name;
 
 	if (choice == 1) {
-		std::cout << "Введите размерность первой матрицы через пробел: ";
+		std::cout << "Р’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂРЅРѕСЃС‚СЊ РїРµСЂРІРѕР№ РјР°С‚СЂРёС†С‹ С‡РµСЂРµР· РїСЂРѕР±РµР»: ";
 		std::cin >> rows_1 >> columns_1;
 		res_row = rows_1;
 		count_of_elems = rows_1 * columns_1;
-		std::cout << "Введите заполненность первой матрицы (процент ненулевых элементов): ";
+		std::cout << "Р’РІРµРґРёС‚Рµ Р·Р°РїРѕР»РЅРµРЅРЅРѕСЃС‚СЊ РїРµСЂРІРѕР№ РјР°С‚СЂРёС†С‹ (РїСЂРѕС†РµРЅС‚ РЅРµРЅСѓР»РµРІС‹С… СЌР»РµРјРµРЅС‚РѕРІ): ";
 		std::cin >> fullness;
 		count_of_elems = count_of_elems * fullness / 100;
 		mat_1 = generate_sparse_mat(rows_1, columns_1, count_of_elems);
 
-		std::cout << "Введите размерность второй матрицы через пробел: ";
+		std::cout << "Р’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂРЅРѕСЃС‚СЊ РІС‚РѕСЂРѕР№ РјР°С‚СЂРёС†С‹ С‡РµСЂРµР· РїСЂРѕР±РµР»: ";
 		std::cin >> rows_2 >> columns_2;
 		res_col = columns_2;
 		count_of_elems = rows_2 * columns_2;
-		std::cout << "Введите заполненность второй матрицы (процент ненулевых элементов): ";
+		std::cout << "Р’РІРµРґРёС‚Рµ Р·Р°РїРѕР»РЅРµРЅРЅРѕСЃС‚СЊ РІС‚РѕСЂРѕР№ РјР°С‚СЂРёС†С‹ (РїСЂРѕС†РµРЅС‚ РЅРµРЅСѓР»РµРІС‹С… СЌР»РµРјРµРЅС‚РѕРІ): ";
 		std::cin >> fullness;
 		count_of_elems = count_of_elems * fullness / 100;
 		mat_2 = generate_sparse_mat(rows_2, columns_2, count_of_elems);
@@ -272,13 +272,13 @@ int main() {
 		result = mult_helper(temp);
 		auto end1 = std::chrono::high_resolution_clock::now();
 		auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(end1 - start1).count();
-		std::cout << "\nПервая матрица: " << std::endl;
+		std::cout << "\nРџРµСЂРІР°СЏ РјР°С‚СЂРёС†Р°: " << std::endl;
 		print_sparse_mat(mat_1, rows_1, columns_1);
-		std::cout << "\nВторая матрица: " << std::endl;
+		std::cout << "\nР’С‚РѕСЂР°СЏ РјР°С‚СЂРёС†Р°: " << std::endl;
 		print_sparse_mat(mat_2, rows_2, columns_2);
-		std::cout << "\nИтоговая матрица: " << std::endl;
+		std::cout << "\nРС‚РѕРіРѕРІР°СЏ РјР°С‚СЂРёС†Р°: " << std::endl;
 		print_sparse_mat(result, res_row, res_col);
-		std::cout << "\nВремя работы алгоритма: " << duration1 << " мкс.\n";
+		std::cout << "\nР’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ Р°Р»РіРѕСЂРёС‚РјР°: " << duration1 << " РјРєСЃ.\n";
 		std::vector<std::vector<int>> crs_result = convert_to_crs_mat(result, res_row, res_col);
 		print_crs_mat(crs_result);
 		file_name = "crs_res.txt";
@@ -286,24 +286,24 @@ int main() {
 
 	}
 	if (choice == 2) {
-		std::cout << "Введите размерность первой матрицы через пробел: ";
+		std::cout << "Р’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂРЅРѕСЃС‚СЊ РїРµСЂРІРѕР№ РјР°С‚СЂРёС†С‹ С‡РµСЂРµР· РїСЂРѕР±РµР»: ";
 		std::cin >> rows_1 >> columns_1;
 		res_row = rows_1;
 		count_of_elems = rows_1 * columns_1;
 
 		file_name = "custom_1.txt";
-		std::cout << "Введите значения для первой матрицы через пробел на каждой строке: \n";
+		std::cout << "Р’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёСЏ РґР»СЏ РїРµСЂРІРѕР№ РјР°С‚СЂРёС†С‹ С‡РµСЂРµР· РїСЂРѕР±РµР» РЅР° РєР°Р¶РґРѕР№ СЃС‚СЂРѕРєРµ: \n";
 		write_custom_to_file(file_name);
 		std::ifstream custom1(file_name);
 		mat_1 = read_custom_from_file(custom1, rows_1, columns_1);
 
-		std::cout << "Введите размерность второй матрицы через пробел: ";
+		std::cout << "Р’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂРЅРѕСЃС‚СЊ РІС‚РѕСЂРѕР№ РјР°С‚СЂРёС†С‹ С‡РµСЂРµР· РїСЂРѕР±РµР»: ";
 		std::cin >> rows_2 >> columns_2;
 		res_col = columns_2;
 		count_of_elems = rows_2 * columns_2;
 
 		file_name = "custom_2.txt";
-		std::cout << "Введите значения для второй матрицы через пробел на каждой строке: \n";
+		std::cout << "Р’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёСЏ РґР»СЏ РІС‚РѕСЂРѕР№ РјР°С‚СЂРёС†С‹ С‡РµСЂРµР· РїСЂРѕР±РµР» РЅР° РєР°Р¶РґРѕР№ СЃС‚СЂРѕРєРµ: \n";
 		write_custom_to_file(file_name);
 		std::ifstream custom2(file_name);
 		mat_2 = read_custom_from_file(custom2, rows_2, columns_2);
@@ -315,13 +315,13 @@ int main() {
 		result = mult_helper(temp);
 		auto end2 = std::chrono::high_resolution_clock::now();
 		auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2).count();
-		std::cout << "Первая матрица: " << std::endl;
+		std::cout << "РџРµСЂРІР°СЏ РјР°С‚СЂРёС†Р°: " << std::endl;
 		print_sparse_mat(mat_1, rows_1, columns_1);
-		std::cout << "\nВторая матрица: " << std::endl;
+		std::cout << "\nР’С‚РѕСЂР°СЏ РјР°С‚СЂРёС†Р°: " << std::endl;
 		print_sparse_mat(mat_2, rows_2, columns_2);
-		std::cout << "\nИтоговая матрица: " << std::endl;
+		std::cout << "\nРС‚РѕРіРѕРІР°СЏ РјР°С‚СЂРёС†Р°: " << std::endl;
 		print_sparse_mat(result, res_row, res_col);
-		std::cout << "\nВремя работы алгоритма: " << duration2 << " мкс.\n";
+		std::cout << "\nР’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ Р°Р»РіРѕСЂРёС‚РјР°: " << duration2 << " РјРєСЃ.\n";
 		std::vector<std::vector<int>> crs_result = convert_to_crs_mat(result, res_row, res_col);
 		print_crs_mat(crs_result);
 		file_name = "crs_res.txt";
